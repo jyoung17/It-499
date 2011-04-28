b38c2591ff995b08e185039a6a086b9bda0ea1e5
jack young <youngbuck14188@jack-youngs-macbook-pro.local>
2011-04-28 15:18:28 -0400

    Initial Commit

---
+++ WeatherMap/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  WeatherMap
+//
+//  Created by jack young on 4/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
---
+++ WeatherMap/WeatherMapAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WeatherMapAppDelegate.h
+//  WeatherMap
+//
+//  Created by jack young on 4/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherMapAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
